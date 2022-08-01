1efafc25d955031ff74c98c3b49ee53537fb133b
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-08-01 14:31:29 -0500

    test: add feature and model tests for total_price of items associated with customer

--- spec/features/customers/show_spec.rb
+++ spec/features/customers/show_spec.rb
@@ -47,4 +47,25 @@ RSpec.describe 'Customer Show Page', type: :feature do
 
         expect(page).to have_content(supermarket_1.name)
     end
+
+    # Story 2 of 3
+    # As a visitor,
+    # When I visit a customer show page,
+    # I see the total price of all of its items
+    it 'shows the total price of all its items' do 
+        supermarket_1 = Supermarket.create!(name: Faker::Address.community, location: Faker::Address.city)
+        customer_1 = supermarket_1.customers.create!(name: Faker::Name.name)
+
+        item_1 = Item.create!(name: "milk", price: 7)
+        item_2 = Item.create!(name: "cabbage", price: 5)
+        item_3 = Item.create!(name: 'crackers', price: 2)
+
+        CustomerItem.create!(customer_id: customer_1.id, item_id: item_1.id)
+        CustomerItem.create!(customer_id: customer_1.id, item_id: item_2.id)
+        CustomerItem.create!(customer_id: customer_1.id, item_id: item_3.id)
+
+        visit customer_path(customer_1) 
+
+        expect(page).to have_content("Total Item Price: $14")
+    end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,4 +6,23 @@ RSpec.describe Customer, type: :model do
     it { should have_many :customer_items }
     it { should have_many(:items).through(:customer_items) }
   end
+
+  describe 'instance methods' do 
+    describe '#total_price' do 
+      it 'returns the total price of items associated with a customer' do 
+        supermarket_1 = Supermarket.create!(name: Faker::Address.community, location: Faker::Address.city)
+        customer_1 = supermarket_1.customers.create!(name: Faker::Name.name)
+
+        item_1 = Item.create!(name: "milk", price: 7)
+        item_2 = Item.create!(name: "cabbage", price: 5)
+        item_3 = Item.create!(name: 'crackers', price: 2)
+
+        CustomerItem.create!(customer_id: customer_1.id, item_id: item_1.id)
+        CustomerItem.create!(customer_id: customer_1.id, item_id: item_2.id)
+        CustomerItem.create!(customer_id: customer_1.id, item_id: item_3.id)
+
+        expect(customer_1.total_price).to eq 14 
+      end
+    end
+  end
 end
