b3e79acab4fd13d359d7e5b94d54022798d13a9f
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-08-01 14:47:26 -0500

    feat: add supermarket name and link to supermarket items index page in the supermarket show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     resources :customers, only: [:show]
+    
+    resources :supermarkets, only: [:show] do 
+      resources :items, only: [:index] 
+    end
 end
